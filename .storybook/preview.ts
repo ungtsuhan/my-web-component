ada3f6c2294cbdbb2dca6bdfc4dccaedbad84c12
ungtsuhan <ungtsuhan@gmail.com>
2025-09-23 16:47:12 +0800

    chore: format code with Prettier

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,11 @@
-import type { Preview } from '@storybook/web-components-vite'
+import type { Preview } from '@storybook/web-components-vite';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
