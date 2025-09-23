8598b792efd88626d9c0018475309e8354effa46
ungtsuhan <ungtsuhan@gmail.com>
2025-09-23 17:16:50 +0800

    chore: setup eslint-plugin-import

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,18 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended);
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
+  plugins: { import: importPlugin },
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+});
