1b28709d720b49dab449fa046d3f25c41c8aa70d
ungtsuhan <ungtsuhan@gmail.com>
2025-09-24 11:19:15 +0800

    chore: ignore dist folder for eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,16 +3,23 @@ import { defineConfig } from 'eslint/config';
 import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
-export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
-  plugins: { import: importPlugin },
-  rules: {
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-        'newlines-between': 'always',
-      },
-    ],
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    plugins: { import: importPlugin },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
+        },
+      ],
+    },
   },
-});
+  {
+    ignores: ['dist/*'],
+  }
+);
